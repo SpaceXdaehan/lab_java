ec091d7710dfff6846e83c44b83750b6b26b60b6
JakeOh <jake.oh.lecture@gmail.com>
2023-03-03 11:13:14 +0900

    conditional

---
+++ Java03_Control/src/edu/java/conditional06/ConditionalMain06.java
@@ -0,0 +1,15 @@
+package edu.java.conditional06;
+
+public class ConditionalMain06 {
+
+    public static void main(String[] args) {
+        // Random 타입의 변수를 선언하고 초기화.
+        // java 과목 점수를 난수(0 ~ 100)로 만들어서 저장.
+        // sql 과목 점수를 난수(0 ~ 100)로 만들어서 저장.
+        // html 과목 점수를 난수(0 ~ 100)로 만들어서 저장.
+        // 합격/불합격 여부를 출력
+        // 합격 조건: 세 과목의 점수가 모두 40점 이상이고, 세 과목의 평균이 60점 이상.
+
+    }
+
+}
