4724368ffb3af8dc050c4391fde3bcc4573a246a
JakeOh <jake.oh.lecture@gmail.com>
2023-03-06 17:28:54 +0900

    loop

---
+++ Java03_Control/src/edu/java/loop08/LoopMain08.java
@@ -0,0 +1,11 @@
+package edu.java.loop08;
+
+public class LoopMain08 {
+
+    public static void main(String[] args) {
+        // 구구단 2단은 2x2까지, 3단은 3x3까지, 4단은 4x4까지, ..., 9단은 9x9까지 출력.
+        
+
+    }
+
+}
