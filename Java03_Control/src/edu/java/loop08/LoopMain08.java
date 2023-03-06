12098c25ec18a18a75f117fc24a085742b2efa49
JakeOh <jake.oh.lecture@gmail.com>
2023-03-06 18:01:58 +0900

    loop

--- Java03_Control/src/edu/java/loop08/LoopMain08.java
+++ Java03_Control/src/edu/java/loop08/LoopMain08.java
@@ -4,7 +4,24 @@ public class LoopMain08 {
 
     public static void main(String[] args) {
         // 구구단 2단은 2x2까지, 3단은 3x3까지, 4단은 4x4까지, ..., 9단은 9x9까지 출력.
+        for (int x = 2; x < 10; x++) {
+            System.out.println("--- " + x + "단 ---");
+            for (int y = 1; y < 10; y++) {
+                System.out.printf("%d x %d = %d\n", x, y, x * y);
+                if (x == y) {
+                    break;
+                } // end if
+            } // end for (y)
+        } // end for (x)
         
+        System.out.println("\n==========\n");
+        
+        for (int x = 2; x < 10; x++) {
+            System.out.println("--- " + x + "단 ---");
+            for (int y = 1; y <= x; y++) {
+                System.out.printf("%d x %d = %d\n", x, y, x * y);
+            }
+        }
 
     }
 
