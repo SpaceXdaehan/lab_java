ce1fa7b8991a4fc9c8886fabf6ca78c916d389bd
JakeOh <jake.oh.lecture@gmail.com>
2023-03-06 18:13:22 +0900

    homework

--- Java03_Control/src/edu/java/loop09/LoopMain09.java
+++ Java03_Control/src/edu/java/loop09/LoopMain09.java
@@ -25,7 +25,8 @@ public class LoopMain09 {
 
         /* 문제 3. 주사위 2개를 던졌을 때 나오는 주사위 눈을 (x, y) 형식으로 출력하세요.
            두 눈의 합이 5가 되면 출력을 멈추세요.
-           힌트: Random 타입의 nextInt(start, end) 메서드를 사용.
+           힌트 1: Random 타입의 nextInt(start, end) 메서드를 사용.
+           힌트 2 무한 루프: for ( ; ; ) {...}, while (true) {...}
            출력 결과 예시:
            (2, 4)
            (6, 1)
