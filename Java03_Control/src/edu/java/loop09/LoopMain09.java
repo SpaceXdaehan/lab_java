d40bb0450fb44f80c61891c4c66b9e6e8ccbec5c
JakeOh <jake.oh.lecture@gmail.com>
2023-03-07 12:25:42 +0900

    loop

--- Java03_Control/src/edu/java/loop09/LoopMain09.java
+++ Java03_Control/src/edu/java/loop09/LoopMain09.java
@@ -1,5 +1,7 @@
 package edu.java.loop09;
 
+import java.util.Random;
+
 public class LoopMain09 {
 
     public static void main(String[] args) {
@@ -89,10 +91,26 @@ public class LoopMain09 {
         }
         
         /*
-         * 문제 3. 주사위 2개를 던졌을 때 나오는 주사위 눈을 (x, y) 형식으로 출력하세요. 두 눈의 합이 5가 되면 출력을 멈추세요. 힌트
-         * 1: Random 타입의 nextInt(start, end) 메서드를 사용. 힌트 2 무한 루프: for ( ; ; ) {...},
-         * while (true) {...} 출력 결과 예시: (2, 4) (6, 1) (3, 3) (4, 1)
+          문제 3. 주사위 2개를 던졌을 때 나오는 주사위 눈을 (x, y) 형식으로 출력하세요. 
+          두 눈의 합이 5가 되면 출력을 멈추세요. 
+          힌트 1: Random 타입의 nextInt(start, end) 메서드를 사용. 
+          힌트 2 무한 루프: for ( ; ; ) {...}, while (true) {...} 
+          출력 결과 예시: (2, 4) (6, 1) (3, 3) (4, 1)
          */
+        System.out.println("\n==========\n");
+        
+        Random rand = new Random();
+        
+        //for (;;) { // 무한 반복문
+        while (true) { // 무한 반복문
+            int dice1 = rand.nextInt(6) + 1;
+            int dice2 = rand.nextInt(1, 7); // 1 <= r < 7
+            System.out.printf("(%d, %d)\n", dice1, dice2);
+            if (dice1 + dice2 == 5) { // 주사위 두 눈의 합이 5가 되면
+                break;
+            }
+        }
+        
     }
 
 }
