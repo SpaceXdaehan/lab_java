76d3f5503c7b92d35fffc80f849858b8106018c2
JakeOh <jake.oh.lecture@gmail.com>
2023-03-07 17:24:55 +0900

    array

---
+++ Java04_Array/src/edu/java/array04/ArrayMain04.java
@@ -0,0 +1,13 @@
+package edu.java.array04;
+
+public class ArrayMain04 {
+
+    public static void main(String[] args) {
+        // 정수 5개를 저장할 수 있는 배열을 선언.
+        // 0 이상 9 이하의 정수 난수 5개를 배열에 저장.
+        // 배열의 모든 원소의 합을 계산하고 출력.
+        // 배열 원소들의 평균을 double 타입으로 계산하고 출력.
+
+    }
+
+}
