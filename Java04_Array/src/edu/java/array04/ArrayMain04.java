f894f651991d81e740155d76538df88642b56c55
JakeOh <jake.oh.lecture@gmail.com>
2023-03-08 11:31:31 +0900

    array

--- Java04_Array/src/edu/java/array04/ArrayMain04.java
+++ Java04_Array/src/edu/java/array04/ArrayMain04.java
@@ -35,6 +35,24 @@ public class ArrayMain04 {
         double average = (double) sum / numbers.length;
         System.out.println("average = " + average);
 
+        // 배열의 원소들 중 최댓값을 찾고 출력.
+        int max = numbers[0]; // 배열의 첫번째(인덱스 0번 위치의) 원소를 최댓값이라고 가정.
+        for (int x : numbers) { // 배열 numbers의 모든 원소들을 순회하면서
+            if (x > max) { // 배열에서 읽은 값이 max보다 크다면
+                max = x; // max의 값을 x로 변경.
+            }
+        }
+        System.out.println("max = " + max);
+        
+        // 배열의 원소들 중 최솟값을 찾고 출력.
+        int min = numbers[0]; // 배열의 첫번째 원소를 최솟값이라고 가정.
+        for (int x : numbers) { // 배열의 모든 원소를 순서대로 반복하면서,
+            if (x < min) { // 배열에서 읽은 값이 min보다 작다면
+                min = x; // min의 값을 x(배열에 읽은 더 작은 값)로 변경.
+            }
+        }
+        System.out.println("min = " + min);
+        
     }
 
 }
