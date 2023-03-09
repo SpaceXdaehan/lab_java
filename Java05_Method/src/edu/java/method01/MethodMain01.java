6f1078bc6da460f5c433d370a3b1d01d988fe127
JakeOh <jake.oh.lecture@gmail.com>
2023-03-09 12:25:18 +0900

    method

--- Java05_Method/src/edu/java/method01/MethodMain01.java
+++ Java05_Method/src/edu/java/method01/MethodMain01.java
@@ -6,6 +6,7 @@ import java.util.Random;
 // - 함수(function): 프로그램에서 사용될 기능을 코드 블록으로 작성한 것.
 // - 메서드: 클래스 안에서 작성된 함수.
 // 자바 언어는 메서드만 제공하고, 함수는 제공하지 않음!
+// 자바 언어는 메서드 안에서 메서드를 정의하는 것을 제공하지 않음!
 
 // 메서드 호출(call, invoke): 메서드를 사용하는 것.
 // argument: 메서드를 호출할 때 메서드에게 전달하는 값.
@@ -15,10 +16,14 @@ import java.util.Random;
 // - println(): 반환 값이 없는 메서드.
 // - nextInt(): 반환 값이 있는 메서드.
 
+// 메서드 정의(선언) 방법:
+// [수식어] 리턴타입 메서드이름([파라미터 선언, ...]) { ... }
+// 메서의 리턴 값이 없는 경우에는 리턴 타입을 void라고 선언함.
 public class MethodMain01 {
 
     // main 메서드(method): 프로그램 시작점.
     public static void main(String[] args) {
+        
         System.out.println("안녕하세요~");
         //-> println() 메서드 호출 -> 호출 결과: 콘솔 창에 문자열을 출력.
 
@@ -27,6 +32,32 @@ public class MethodMain01 {
         //-> nextInt() 메서드(기능) 호출 -> nextInt() 메서드는 난수를 반환(return).
         System.out.println(x);
         
+        System.out.println("테스트1");
+        newLine(); // 같은 클래스에 정의(선언)된 메서드를 호출.
+        System.out.println("테스트2");
+        
+        System.out.println("테스트3");
+        newLine(3);
+        System.out.println("테스트4");
+        
     }
-
+    
+    /**
+     * 콘솔에 빈 줄 하나를 출력하는 메서드.
+     */
+    public static void newLine() {
+        System.out.println();
+    }
+    
+    /**
+     * 콘솔에 n개의 빈 줄을 출력하는 메서드.
+     * 
+     * @param n 0 이상의 정수. 출력할 빈 줄의 개수.
+     */
+    public static void newLine(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+        }
+    }
+        
 }
