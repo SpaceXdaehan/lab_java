bce7f3e1284b01c7045292fe560dddcd560623c4
JakeOh <jake.oh.lecture@gmail.com>
2023-03-13 15:20:40 +0900

    constructor

---
+++ Java06_Class/src/edu/java/class03/ClassMain03.java
@@ -0,0 +1,12 @@
+package edu.java.class03;
+
+public class ClassMain03 {
+
+    public static void main(String[] args) {
+        // User 타입 변수 선언, User 타입(클래스) 객체를 생성하고 저장.
+        User user1 = new User(); // 기본 생성자 호출 -> User 객체 생성.
+        user1.printUserInfo();
+
+    }
+
+}
