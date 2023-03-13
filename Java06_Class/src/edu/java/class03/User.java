61973f4f83cb1d9803f0d4a72c28c2bdb2dc0278
JakeOh <jake.oh.lecture@gmail.com>
2023-03-13 17:13:22 +0900

    OOP

--- Java06_Class/src/edu/java/class03/User.java
+++ Java06_Class/src/edu/java/class03/User.java
@@ -22,6 +22,32 @@ public class User {
     int userAge;
     
     // constructor
+    public User() {
+        // 코드를 작성하지 않아도, 각 필드들을 그 타입의 기본값으로 초기화를 해줌.
+//        this.userId = null;
+//        this.userPassword = null;
+//        this.userAge = 0;
+    }
+    
+    public User(String userId, String userPassword, int userAge) {
+        // field를 초기화
+        // this - field userId와 parameter userId를 구별하기 위해서 사용.
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.userAge = userAge;
+    }
+    
+    public User(String userId, String userPassword) {
+//        this.userId = userId;
+//        this.userPassword = userPassword;
+//        this.userAge = 0;
+        this(userId, userPassword, 0);
+    }
+    
+    // 생성자 오버로딩(overloading): 파라미터가 다른 생성자를 여러개 정의하는 것.
+    // this의 의미(기능):
+    // (1) 생성된 객체(인스턴스) - 인스턴스의 필드와 지역 변수를 구별하기 위한 용도.
+    // (2) overloading된 다른 생성자 호출.
     
     // method
     public void printUserInfo() {
