a1ed3220225bc5da3b8ec1b0ddb887c35fbb61af
JakeOh <jake.oh.lecture@gmail.com>
2023-03-14 11:30:11 +0900

    OOP

---
+++ Java06_Class/src/edu/java/class06/Circle.java
@@ -0,0 +1,18 @@
+package edu.java.class06;
+
+// 원(동그라미)
+public class Circle {
+    // field - 객체의 속성
+    double radius; // 원의 반지름
+    
+    // 생성자 - 객체 속성을 초기화
+    // (1) 기본 생성자
+    // (2) argument 1개를 갖는 생성자
+    
+    
+    // 메서드 - 객체의 기능
+    // (1) printInfo - 원의 반지름 정보를 출력.
+    // (2) perimeter - 원의 둘레 길이(2 x 3.14 x 반지름)을 리턴.
+    // (3) area - 원의 넓이(3.14 x 반지름 x 반지름)을 리턴.
+
+}
---
+++ Java06_Class/src/edu/java/class06/ClassMain06.java
@@ -0,0 +1,10 @@
+package edu.java.class06;
+
+public class ClassMain06 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
