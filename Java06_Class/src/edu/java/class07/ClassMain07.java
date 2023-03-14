a25c432941ca9acf45bd8a0e40f8925c806cc067
JakeOh <jake.oh.lecture@gmail.com>
2023-03-14 15:37:20 +0900

    OOP

--- Java06_Class/src/edu/java/class07/ClassMain07.java
+++ Java06_Class/src/edu/java/class07/ClassMain07.java
@@ -3,8 +3,18 @@ package edu.java.class07;
 public class ClassMain07 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        // 기본 생성자를 사용해서 Score 객체 생성.
+        Score score1 = new Score();
+        System.out.println(score1);
+        score1.printScore();
 
+        // argument를 갖는 생성자를 호출해서 Score 객체를 생성.
+        Score score2 = new Score(100, 90, 94);
+        System.out.println(score2);
+        score2.printScore();
+        System.out.println("score2 총점: " + score2.getTotal());
+        System.out.println("score2 평균: " + score2.getAverage());
+        
     }
 
 }
