238343c2dc3f4ae7b343c04de2aa31eec4c29fb0
JakeOh <jake.oh.lecture@gmail.com>
2023-03-16 10:20:47 +0900

    instance, static

---
+++ Java06_Class/src/edu/java/modifier07/Test.java
@@ -0,0 +1,11 @@
+package edu.java.modifier07;
+
+public class Test {
+    int x; // 인스턴스 필드
+    
+    // 인스턴스 메서드
+    public void printInfo() {
+        System.out.println("x = " + this.x);
+    }
+    
+}
