0c2dbc40fa8ba4c1851a15cba2b94bf45f902579
JakeOh <jake.oh.lecture@gmail.com>
2023-03-20 11:23:01 +0900

    inheritance, override, polymorphism

--- Java07_Inheritance/src/edu/java/inheritance03/HybridCar.java
+++ Java07_Inheritance/src/edu/java/inheritance03/HybridCar.java
@@ -14,4 +14,20 @@ public class HybridCar extends Car {
         this.battery = battery;
     }
     
+    @Override // 애너테이션(annotation)
+    public void drive() {
+//        System.out.printf("하이브리드 자동차 운전: 속력=%f, 연료=%f, 배터리=%d", 
+//                getSpeed(), getFuel(), this.battery);
+        super.drive(); // 상위 타입 객체가 가지고 있는 (override 전) 메서드를 호출.
+        System.out.println("--- 배터리=" + battery);
+    }
+    
+    // 메서드/생성자 overloading:
+    // 파라미터가 다를 때, 같은 이름으로 메서드/생성자를 여러개 작성하는 것.
+    
+    // 메서드 overriding:
+    // 상위 클래스의 메서드를 하위 클래스에서 재정의하는 것.
+    // 리턴 타입, 메서드 이름, 파라미터 선언이 모두 같아야 함.
+    // 접근 제한 수식어는 상위 클래스의 수식어보다 범위가 넓거나 같으면 됨.
+    
 }
--- Java07_Inheritance/src/edu/java/inheritance03/InheritanceMain03.java
+++ Java07_Inheritance/src/edu/java/inheritance03/InheritanceMain03.java
@@ -10,6 +10,13 @@ public class InheritanceMain03 {
         // HybridCar 타입의 객체 생성.
         HybridCar car2 = new HybridCar(90, 50, 100);
         car2.drive();
+        
+        // 다형성(polymorphism): 하나의 객체를 두 개 이상의 타입으로 부를 수 있는 것.
+        // 객체를 자기 자신 타입 뿐만 아니라 그 "상위 타입"의 변수에도 할당할 수 있는 것. 
+        // 하이브리드 자동차는 "하이브리드 자동차"이다.
+        // 하이브리드 자동차는 "자동차"이다.
+        Car car3 = new HybridCar(100, 50, 100);
+        car3.drive();
 
     }
 
