11494839671cb631b76e29fa3b7bb0d964061a75
JakeOh <jake.oh.lecture@gmail.com>
2023-03-20 12:20:14 +0900

    inheritance, polymorphism, casting

--- Java07_Inheritance/src/edu/java/inheritance03/InheritanceMain03.java
+++ Java07_Inheritance/src/edu/java/inheritance03/InheritanceMain03.java
@@ -16,8 +16,23 @@ public class InheritanceMain03 {
         // 하이브리드 자동차는 "하이브리드 자동차"이다.
         // 하이브리드 자동차는 "자동차"이다.
         Car car3 = new HybridCar(100, 50, 100);
-        car3.drive();
+        car3.drive(); //-> 다형성이 적용된 경우에도, override되어 있는 메서드가 실행됨.
 
+        // 다형성은 코드의 재사용성(reuse)을 높여줌.
+        // 변수 선언, 배열/리스트 선언, 파라미터 선언, 메서드 리턴 타입 선언, ...
+        // SuperType var = new SuperType();
+        // SuperType var = new SubType();
+        // SuperType[] arr = {new SuperType(), new SubType()}
+        Car[] cars = {car1, car2, car3};
+        for (Car c : cars) {
+            c.drive();
+        }
+        
+        // 다형성을 사용해서 정의된 메서드: 
+        // System.out.println(Object o) - 어떤 타입의 객체도 모두 argument로 전달할 수 있음.
+        System.out.println(car1);
+        System.out.println(car2);
+        
     }
 
 }
