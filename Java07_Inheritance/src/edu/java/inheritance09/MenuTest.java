9be407c90c9b2c2c6528d741e7d1ef30098ff9c0
JakeOh <jake.oh.lecture@gmail.com>
2023-03-21 18:02:22 +0900

    enum

--- Java07_Inheritance/src/edu/java/inheritance09/MenuTest.java
+++ Java07_Inheritance/src/edu/java/inheritance09/MenuTest.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 
 enum Menu {
     // enum 상수(public static final 변수) 선언
-    QUIT, CREATE, READ_ALL, READ_BY_INDEX, UPDATE, DELETE;
+    QUIT, CREATE, READ_ALL, READ_BY_INDEX, UPDATE, DELETE, UNKONWN;
+
+    /**
+     * 정수가 입력되면, 그 정수에 해당하는 열거형 상수를 리턴.
+     * 0 -> QUIT, 1 -> CREATE, 2 -> READ_ALL, 3 -> READ_BY_INDES,
+     * 4 -> UPDATE, 5 -> DELETE, 그 이외의 정수들인 경우에는 UNKNOWN을 리턴.
+     * @param n 정수(int).
+     * @return Menu 타입의 열거형 상수.
+     */
+    public static Menu getValue(int n) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
 
 public class MenuTest {
