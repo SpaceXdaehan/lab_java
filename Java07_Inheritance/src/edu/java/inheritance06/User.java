51893639d8cc20e278fce32ed45b7c65d7df5168
JakeOh <jake.oh.lecture@gmail.com>
2023-03-21 10:25:17 +0900

    toString, equals, hashCode

--- Java07_Inheritance/src/edu/java/inheritance06/User.java
+++ Java07_Inheritance/src/edu/java/inheritance06/User.java
@@ -6,12 +6,52 @@ public class User {
     private String password;
     
     // 생성자: (1) 기본 생성자, (2) argument 2개를 갖는 생성자.
+    public User() {}
+    
+    public User(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
     
     // getters & setters
+    public String getUserId() {
+        return this.userId;
+    }
+    
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
     // toString() override: "User(userId=..., password=...)"
+    @Override
+    public String toString() {
+        // return String.format("User(userId=%s, password=%s)", this.userId, this.password);
+        return "User(userId=" + this.userId
+                + ", password=" + this.password
+                + ")";
+    }
     
     // equals() override: 두 객체의 userId가 같으면 true, 그렇지 않으면 false. 
     // hashCode() override
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+        if (obj instanceof User) {
+            User other = (User) obj;
+            if (this.userId != null && this.userId.equals(other.userId)) {
+                result = true;
+            }
+        }
+        
+        return result;
+    }
 
 }
