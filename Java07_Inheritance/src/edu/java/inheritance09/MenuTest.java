c013017cb83cef5bedd075399c738ef6780deb54
JakeOh <jake.oh.lecture@gmail.com>
2023-03-22 10:05:44 +0900

    enum

--- Java07_Inheritance/src/edu/java/inheritance09/MenuTest.java
+++ Java07_Inheritance/src/edu/java/inheritance09/MenuTest.java
@@ -3,19 +3,24 @@ package edu.java.inheritance09;
 import java.util.Scanner;
 
 enum Menu {
-    // enum 상수(public static final 변수) 선언
+    // enum 열거형 상수(public static final Menu 타입 변수) 선언
     QUIT, CREATE, READ_ALL, READ_BY_INDEX, UPDATE, DELETE, UNKONWN;
 
     /**
      * 정수가 입력되면, 그 정수에 해당하는 열거형 상수를 리턴.
-     * 0 -> QUIT, 1 -> CREATE, 2 -> READ_ALL, 3 -> READ_BY_INDES,
+     * 0 -> QUIT, 1 -> CREATE, 2 -> READ_ALL, 3 -> READ_BY_INDEX,
      * 4 -> UPDATE, 5 -> DELETE, 그 이외의 정수들인 경우에는 UNKNOWN을 리턴.
      * @param n 정수(int).
      * @return Menu 타입의 열거형 상수.
      */
     public static Menu getValue(int n) {
-        // TODO Auto-generated method stub
-        return null;
+        Menu[] menu = values(); // Menu.values()
+        int len = menu.length;
+        if (n >= 0 && n < len) {
+            return menu[n];
+        } else {
+            return menu[len - 1]; // return UNKNONWN;
+        }
     }
 
 }
@@ -23,7 +28,13 @@ enum Menu {
 public class MenuTest {
     
     public static void main(String[] args) {
+        Singleton s = Singleton.INSTANCE;
+        //-> enum 타입이 열거형 상수를 1개만 가지고 있으면,
+        // 그 enum 타입에 할당할 수 있는 객체는 오직 1개만 있게 됨.
+        //-> 싱글톤 객체
+        
         Scanner sc = new Scanner(System.in);
+        System.out.print("정수 입력> ");
         int n = Integer.parseInt(sc.nextLine()); // 콘솔창에서 입력받은 정수
         
 //        Menu m = Menu.CREATE;
@@ -47,6 +58,8 @@ public class MenuTest {
         case DELETE:
             System.err.println(menu.name());
             break;
+        default:
+            System.out.println(menu.name());
         }
 
     }
