f7aefa48c5299b64355a595f30ad11b55f76d27a
JakeOh <jake.oh.lecture@gmail.com>
2023-03-22 10:24:34 +0900

    interface

---
+++ Java08_Interface/src/edu/java/interface01/InterfaceMain01.java
@@ -0,0 +1,20 @@
+package edu.java.interface01;
+
+
+// 인터페이스(interface):
+// 사용 목적: 팀/회사 간의 분업/협업을 하기 위해서 메서드(기능)들의 프로토타입(prototype, signature)을 약속(정의)하는 것.
+// 인터페이스가 가질 수 있는 멤버들:
+//   (1) public static final 필드. 필드 선언할 때 보통 public static final은 생략 가능.
+//   (2) public abstract 메서드. 메서드 선언할 때 보통 public abstract는 생략 가능.
+// 인터페이스를 작성할 때는 interface 키워드를 사용.
+// 인터페이스는 객체를 생성할 수 없음.
+// 인터페이스를 "구현"하는 클래스를 작성하고, 그 클래스의 객체를 생성함.
+
+public class InterfaceMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
