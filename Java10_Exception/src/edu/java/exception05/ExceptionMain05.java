fe7730a08c28e4b000410a664ff18becbc9ce15f
JakeOh <jake.oh.lecture@gmail.com>
2023-03-23 17:22:34 +0900

    try-catch

--- Java10_Exception/src/edu/java/exception05/ExceptionMain05.java
+++ Java10_Exception/src/edu/java/exception05/ExceptionMain05.java
@@ -10,7 +10,7 @@ package edu.java.exception05;
  * 2. checked exception:
  *    - 반드시 예외 처리를 해야만 하는 예외 클래스들.
  *    - try-catch 문장을 사용하지 않거나 또는 throws 선언문을 사용하지 않으면 컴파일 에러가 발생하는 예외 클래스들.
- *    - RuntimeException의 하위 클래스가 아닌 Exception 하위 클래스들.
+ *    - RuntimeException의 하위 클래스가 아닌 Exception과 그 하위 클래스들.
  *    - (예) Exception, IOException, FileNotFoundException, ...
  */
 
--- Java10_Exception/src/edu/java/exception06/ExceptionMain06.java
+++ Java10_Exception/src/edu/java/exception06/ExceptionMain06.java
@@ -7,15 +7,27 @@ public class ExceptionMain06 {
     private Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // TODO inputInteger() 메서드 테스트 코드
-
+        // inputInteger() 메서드 테스트 코드
+        ExceptionMain06 app = new ExceptionMain06();
+        int x = app.inputInteger();
+        System.out.println("x = " + x);
     }
 
     public int inputInteger() {
-        // TODO Scanner를 사용해서 입력받은 정수를 리턴.
-        // Integer.parseInt(scanner.nextLine()) 과정에서 NumberFormatException이 발생할 수 있음.
+        // Scanner를 사용해서 입력받은 정수를 리턴.
+        // Integer.parseInt(scanner.nextLine()) 과정에서 NumberFormatException을 처리.
+        int result = 0; // 콘솔에서 입력받은 값을 저장할 변수
+        while (true) {
+            try {
+                System.out.print("정수 입력> ");
+                result = Integer.parseInt(scanner.nextLine());
+                break; // 반복문을 끝냄.
+            } catch (NumberFormatException e) {
+                System.out.println("정수로 입력해주세요...");
+            }
+        }
         
-        return 0;
+        return result;
     }
     
 }
