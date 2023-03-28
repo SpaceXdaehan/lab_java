2c74ff1f127bf7892caac5287a8eac6bc28ad88e
JakeOh <jake.oh.lecture@gmail.com>
2023-03-28 16:23:35 +0900

    file

---
+++ Java13_File/src/edu/java/file01/FileMain01.java
@@ -0,0 +1,14 @@
+package edu.java.file01;
+
+/*
+ * 입/출력 스트림(Input/Output Stream): 프로그램이 값(데이터)을 입력받거나 출력하는 통로.
+ *   (예) System.in: 입력 스트림 객체, System.out: 출력 스트림 객체.
+ */
+
+public class FileMain01 {
+
+    public static void main(String[] args) {
+        System.out.println(System.in);
+    }
+    
+}
