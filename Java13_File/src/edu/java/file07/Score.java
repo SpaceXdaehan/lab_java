3eeff489256963804f1b0e173be5a253ac934f45
JakeOh <jake.oh.lecture@gmail.com>
2023-03-29 17:17:57 +0900

    ObjectInput/OutputStream, BufferedInput/OutputStream, FileInput/OutputStream

---
+++ Java13_File/src/edu/java/file07/Score.java
@@ -0,0 +1,9 @@
+package edu.java.file07;
+
+public class Score {
+    private int java;
+    private int web;
+    private int python;
+
+    // TODO: 생성자, getters, setters, toString
+}
---
+++ Java13_File/src/edu/java/file07/Student.java
@@ -0,0 +1,10 @@
+package edu.java.file07;
+
+public class Student {
+    private int id;
+    private String name;
+    private Score score;
+    
+    // TODO: 생성자, getters, setters, toString
+
+}
