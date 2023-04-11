23b7c50b8218a454673f15a51af95f1567e2c3ff
JakeOh <jake.oh.lecture@gmail.com>
2023-04-11 10:20:38 +0900

    Add files via upload

--- lab-oracle/sql06_join.sql
+++ lab-oracle/sql06_join.sql
@@ -17,10 +17,54 @@ join 종류:
       - full outer join
 */
 
+-- inner join과 outer join의 차이점을 보기 위해서.
+insert into emp (empno, ename, deptno)
+values (1004, '오쌤', 50);
+
+commit;
+
 -- 사번, 이름, 부서번호, 부서이름을 검색. inner join.
+select e.empno, e.ename, e.deptno, d.dname
+from emp e
+    inner join dept d on e.deptno = d.deptno;
 
+select e.empno, e.ename, e.deptno, d.dname
+from emp e, dept d
+where e.deptno = d.deptno;
 
+-- left outer join
+select e.empno, e.ename, e.deptno, d.dname
+from emp e
+    left outer join dept d on e.deptno = d.deptno;
 
+select e.empno, e.ename, e.deptno, d.dname
+from emp e, dept d
+where e.deptno = d.deptno(+);
 
+-- right outer join
+select e.empno, e.ename, d.deptno, d.dname
+from emp e
+    right outer join dept d on e.deptno = d.deptno;
 
+select e.empno, e.ename, d.deptno, d.dname
+from emp e, dept d
+where e.deptno(+) = d.deptno;
+
+-- full outer join
+select e.empno, e.ename, e.deptno, d.deptno, d.dname
+from emp e
+    full outer join dept d on e.deptno = d.deptno;
+
+-- Oracle에서는 full outer join 문법을 제공하지 않음.
+-- left outer join 결과와 right outer join 결과를 합집합(union)하면 됨.
+select e.empno, e.ename, e.deptno, d.deptno, d.dname
+from emp e, dept d
+where e.deptno = d.deptno(+) -- left outer join
+union                        -- 합집합
+select e.empno, e.ename, e.deptno, d.deptno, d.dname
+from emp e, dept d
+where e.deptno(+) = d.deptno; -- right outer join
+
+-- inner join에서 inner는 생략 가능. inner join = join
+-- left/right/full outer join에서 outer 생략가능함. left outer join = left join
 
