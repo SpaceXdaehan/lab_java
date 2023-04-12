1e3a3dcd6eecc313d06daedc98bd8839f85b68a8
JakeOh <jake.oh.lecture@gmail.com>
2023-04-12 10:25:36 +0900

    Add files via upload

---
+++ lab-oracle/sql08_constraint.sql
@@ -0,0 +1,18 @@
+-- 제약조건(constraint):
+-- 테이블에 데이터들을 insert할 때, 조건을 만족하는 값들만 insert될 수 있도록 만들어 주는 기능.
+-- 테이블을 생성(create table)할 때 제약조건을 정의.
+-- 테이블을 변경(alter table)할 때 제약조건을 추가/변경/삭제.
+-- 종류: primary key, not null, unique, check, foreign key
+-- 테이블 생성할 때 제약조건 이름을 설정하지 않으면, 시스템에서 자동으로 이름을 부여.
+--   (예) SYS_C008813 -> insert할 때 제약조건에 위배가 되면 에러 메시지에서 볼 수 있음.
+
+
+
+
+
+
+
+
+
+
+
