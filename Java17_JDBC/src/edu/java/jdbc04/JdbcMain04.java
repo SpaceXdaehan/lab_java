1fdf8ad5b36db4a0f3c169c3d6dcf84c6d683e5f
JakeOh <jake.oh.lecture@gmail.com>
2023-04-13 17:26:12 +0900

    JDBC delete

---
+++ Java17_JDBC/src/edu/java/jdbc04/JdbcMain04.java
@@ -0,0 +1,11 @@
+package edu.java.jdbc04;
+
+public class JdbcMain04 {
+
+    public static void main(String[] args) {
+        // JDBC delete:
+        // delete from BLOGS where ID = ?
+
+    }
+
+}
