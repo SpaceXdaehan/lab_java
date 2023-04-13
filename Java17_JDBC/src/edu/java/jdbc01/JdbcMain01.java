24dbb8d63080b51e0a906baf013929236ee774e7
JakeOh <jake.oh.lecture@gmail.com>
2023-04-13 12:22:45 +0900

    JDBC

---
+++ Java17_JDBC/src/edu/java/jdbc01/JdbcMain01.java
@@ -0,0 +1,24 @@
+package edu.java.jdbc01;
+
+/*
+ * JDBC(Java Database Connectivity):
+ * 자바 프로그램에서 데이터베이스를 연결하고, SQL 문장들을 실행하는 방법.
+ * 
+ * 1. Oracle JDBC 라이브러리를 다운로드하고 자바 프로젝트에 라이브러리를 포함.
+ *    (1) 라이브러리 다운로드.
+ *    (2) 이클립스 프로젝트 폴더에 lib 폴더 생성.
+ *    (3) 다운로드한 라이브러리 jar 파일을 lib 폴더에 복사.
+ *    (4) jar 파일을 프로젝트의 빌드 패스(build path)에 추가.
+ * 2. 오라클 데이터베이스 서버에 접속하기 위한 정보들(URL, USER, PASSWORD)을 상수로 정의.
+ * 
+ * 
+ */
+
+public class JdbcMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
